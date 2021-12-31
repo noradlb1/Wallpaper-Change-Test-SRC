246aef104b68961141feee765262de820165b4c5
noradlb1 <zcyfkhuru.bz@mail.ru>
2021-12-31 13:56:40 +0100

    Wallpaper-Change-Test-SRC
    Wallpaper-Change-Test-SRC

---
+++ WallpaperChange/Form1.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.RemoverToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.ListView2 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ContextMenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RemoverToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(138, 28)
+        '
+        'RemoverToolStripMenuItem
+        '
+        Me.RemoverToolStripMenuItem.Name = "RemoverToolStripMenuItem"
+        Me.RemoverToolStripMenuItem.Size = New System.Drawing.Size(137, 24)
+        Me.RemoverToolStripMenuItem.Text = "Remover"
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "Trash Full.png")
+        '
+        'ListView2
+        '
+        Me.ListView2.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader2})
+        Me.ListView2.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.ListView2.Location = New System.Drawing.Point(3, 44)
+        Me.ListView2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.RightToLeftLayout = True
+        Me.ListView2.Size = New System.Drawing.Size(354, 182)
+        Me.ListView2.TabIndex = 6
+        Me.ListView2.UseCompatibleStateImageBehavior = False
+        Me.ListView2.View = System.Windows.Forms.View.SmallIcon
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "الصور"
+        Me.ColumnHeader2.Width = 66
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(93, 234)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(112, 32)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "إظافة الصور"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 6000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(367, 200)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(16, 17)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "0"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(211, 234)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(146, 32)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "عرض الصور"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"5 ثواني", "10 ثواني", "30 ثواني", "1 دقيقة", "3 دقيقة", "5 دقيقة", "10 دقيقة", "20 دقيقة", "30 دقيقة"})
+        Me.ComboBox1.Location = New System.Drawing.Point(3, 13)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.ComboBox1.Size = New System.Drawing.Size(215, 24)
+        Me.ComboBox1.TabIndex = 10
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Location = New System.Drawing.Point(224, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(133, 24)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "وقت تغير الخلفيه"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(363, 40)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(240, 218)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(8, 234)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(79, 32)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "حذف الكل"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(615, 275)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.ListView2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "تغير خلفية سطح المكتب"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents RemoverToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents ListView2 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ WallpaperChange/Form1.vb
@@ -0,0 +1,179 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports System.Drawing.Imaging
+Imports System.IO
+Imports System.Text
+Public Class Form1
+
+    Private Declare Function SystemParametersInfo Lib "user32" Alias "SystemParametersInfoA" (ByVal _
+    uAction As Integer, ByVal uParam As Integer, ByVal lpvParam As String, ByVal fuWinIni As Integer) As Integer
+    Private Const SPI_SETDESKWALLPAPER = 20
+    Private Const SPIF_UPDATEINIFILE = &H1
+    Dim filename As String
+    Dim imageCollection As New SortedList
+
+    Dim locall As String = "C:\Users\" & Environment.UserName & "\AppData\Local\Wg2.txt"
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+        Dim myFile As String = locall
+        Dim myWriter As New IO.StreamWriter(myFile)
+        For Each myItem As ListViewItem In ListView2.Items
+
+
+            myWriter.WriteLine(myItem.Text)
+        Next
+
+        myWriter.Close()
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ComboBox1.Text = "10 Segundos"
+        If IO.File.Exists(locall) Then
+
+        Else
+            Dim sw As StreamWriter = File.CreateText(locall)
+
+        End If
+
+
+        On Error Resume Next
+        Using sr As IO.StreamReader = File.OpenText(locall)
+            While (-1 < sr.Peek())
+                Dim line As String = sr.ReadLine()
+                Dim item As New ListViewItem(line.Split("|"))
+                ListView2.Items.Add(item)
+            End While
+            sr.Close()
+        End Using
+    End Sub
+
+    Private Sub RemoverToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RemoverToolStripMenuItem.Click
+        Dim lst As New List(Of Object)
+        For Each a As Object In ListView2.SelectedItems
+            lst.Add(a)
+        Next
+        For Each a As Object In lst
+            ListView2.Items.Remove(a)
+        Next
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim myFile As String = locall
+        Dim myWriter As New IO.StreamWriter(myFile)
+        For Each myItem As ListViewItem In ListView2.Items
+
+
+            myWriter.WriteLine(myItem.Text)
+        Next
+
+        myWriter.Close()
+    End Sub
+
+
+   
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim openDlg As New OpenFileDialog
+        openDlg.Multiselect = True
+
+        Dim path As String
+
+        openDlg.Filter = "Image Files (*.jpg, *.jpeg, *.png, *bmp, *.gif|*.jpg; *.jpeg; *.png; *bmp; *.gif"
+
+        If openDlg.ShowDialog = Windows.Forms.DialogResult.OK Then
+
+            For Each path In openDlg.FileNames
+
+                ListView2.Items.Add(path)
+            Next
+            ' ButtonX1.Enabled = True
+            'onoff.Enabled = False
+            ' عرض الصور()
+            Timer1.Start()
+        Else
+            ' onoff.Value = False
+        End If
+    End Sub
+    Dim somar As Integer = 0
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        '10:     ثواني()
+        '30:     ثواني()
+        '1:      دقيقة()
+        '3:      دقيقة()
+        '5:      دقيقة()
+        '10:     دقيقة()
+        '20:     دقيقة()
+        '30:     دقيقة()
+        If ComboBox1.Text = "5 Segundos" Then
+            Timer1.Interval = 5000
+        End If
+        If ComboBox1.Text = "10 Segundos" Then
+            Timer1.Interval = 10000
+        End If
+
+        If ComboBox1.Text = "30 Segundos" Then
+            Timer1.Interval = 30000
+        End If
+        If ComboBox1.Text = "1 Minuto" Then
+            Timer1.Interval = 600000
+        End If
+        If ComboBox1.Text = "3 Minutos" Then
+            Timer1.Interval = 1800000
+        End If
+        If ComboBox1.Text = "5 Minutos" Then
+            Timer1.Interval = 3000000
+        End If
+        If ComboBox1.Text = "10 Minutos" Then
+            Timer1.Interval = 6000000
+        End If
+        If ComboBox1.Text = "20 Minutos" Then
+            Timer1.Interval = 12000000
+        End If
+        If ComboBox1.Text = "30 Minutos" Then
+            Timer1.Interval = 18000000
+        End If
+        Dim countt As Integer = ListView2.Items.Count
+
+        On Error Resume Next
+        somar += 1
+        Label1.Text = somar
+        'If ListView2.Items(somar).Selected Then
+        On Error Resume Next
+        PictureBox1.ImageLocation = ListView2.Items(somar).Text
+        Dim caminhoImagem As String = Application.StartupPath & "\novo.bmp"
+
+        PictureBox1.Image.Save(caminhoImagem, ImageFormat.Bmp)
+
+        SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, caminhoImagem, SPIF_UPDATEINIFILE)
+        'Exit For
+        If somar = countt Then
+            somar = 0
+        End If
+        'End If
+    End Sub
+    
+
+    Private Sub ListView2_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ListView2.SelectedIndexChanged
+        For i As Integer = 0 To ListView2.Items.Count - 1
+            If ListView2.Items(i).Selected Then
+                PictureBox1.ImageLocation = ListView2.Items(i).Text
+                'PictureBox1.ImageLocation = ListView2.Items(somar).Text
+                Dim caminhoImagem As String = Application.StartupPath & "\novo.bmp"
+                On Error Resume Next
+                PictureBox1.Image.Save(caminhoImagem, ImageFormat.Bmp)
+
+                SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, caminhoImagem, SPIF_UPDATEINIFILE)
+            End If
+        Next
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        ListView2.Items.Clear()
+    End Sub
+End Class
